be85dc5f3fcb141a0f2db52823eda3114020e466
James Guo <g.james@wustl.edu>
2021-12-03 11:36:44 +0800

    fixed

--- functions/api/a.js
+++ functions/api/a.js
@@ -1,8 +1,11 @@
 export async function onRequest(request) {
-  const url = new URL(request.url)
-  url.protocol = 'https:'
-  url.hostname = 'matomo.tloxygen.com'
-  url.pathname = '/matomo.php'
-  fetch(url.toString(), request, { cf: { resolveOverride: url.hostname } })
-  return new Response(null, { status: 204 })
+  try {
+    const url = new URL(request.url)
+    url.protocol = 'https:'
+    url.hostname = 'matomo.tloxygen.com'
+    url.pathname = '/matomo.js'
+    return fetch(url.toString(), request, { cf: { resolveOverride: url.hostname } })
+  } catch (err) {
+    return new Response(`${err.message}\n${err.stack}`, { status: 500 })
+  }
 }
--- functions/api/b.js
+++ functions/api/b.js
@@ -1,7 +1,11 @@
 export async function onRequest(request) {
-  const url = new URL(request.url)
-  url.protocol = 'https:'
-  url.hostname = 'matomo.tloxygen.com'
-  url.pathname = '/matomo.js'
-  return fetch(url.toString(), request, { cf: { resolveOverride: url.hostname } })
+  try {
+    const url = new URL(request.url)
+    url.protocol = 'https:'
+    url.hostname = 'matomo.tloxygen.com'
+    url.pathname = '/matomo.php'
+    return fetch(url.toString(), request, { cf: { resolveOverride: url.hostname } })
+  } catch (err) {
+    return new Response(`${err.message}\n${err.stack}`, { status: 500 })
+  }
 }
